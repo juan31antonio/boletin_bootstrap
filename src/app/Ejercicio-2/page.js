b3d0fd2124ae450095243e884b09a54c120df352
juan31antonio <juanantoniocorder32@gmail.com>
2025-01-27 10:57:50 +0100

    Ejercicios por terminar de bootstrap

---
+++ src/app/Ejercicio-2/page.js
@@ -0,0 +1,16 @@
+export default function Grid() {
+    return (
+      <div className="container">
+        <div className="row">
+            <div className="col-md-4">Columna 1</div>
+            <div className="col-md-4">Columna 2</div>
+            <div className="col-md-4">Columna 3</div>
+        </div>
+        <div className="row">
+            <div className="col-md-8">Columna 1</div>
+            <div className="col-md-4">Columna 2</div>
+        </div>
+      </div>
+    );
+  }
+  
