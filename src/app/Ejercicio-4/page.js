942cb708c4f2f700f07560f68fb956515447c89f
juan31antonio <juanantoniocorder32@gmail.com>
2025-01-28 11:10:31 +0100

    Boletin terminado

--- src/app/Ejercicio-4/page.js
+++ src/app/Ejercicio-4/page.js
@@ -1,7 +1,12 @@
 export default function Home() {
     return (
-      <div>
-        
+      <div className="card" style={{width: "18rem"}}>
+        <img className="card-img-top" src="Bosque.jpg" alt="Card image cap"></img>
+        <div className="card-body">
+          <h5 className="card-title">Explora la Naturaleza</h5>
+          <p className="card-text">Descubre los lugares más impresionantes para conectarte con la naturaleza. Aventuras al aire libre y tranquilidad aseguradas.</p>
+          <button className="btn btn-primary">Ver más</button>
+        </div>
       </div>
     );
   }
--- src/app/Ejercicio-5/page.js
+++ src/app/Ejercicio-5/page.js
@@ -1,8 +1,32 @@
+"use client"
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 export default function Home() {
-    return (
-      <div>
-        
+  return (
+    <>
+      <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        Lanzar el modal
+      </button>
+
+      <div className="modal fade" id="exampleModal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div className="modal-dialog" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLabel">Este es el titulo del modal</h5>
+              <button type="button" className="close" data-bs-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div className="modal-body">
+              Esto es el cuerpo del modal
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              <button type="button" className="btn btn-primary" >Save changes</button>
+            </div>
+          </div>
+        </div>
       </div>
-    );
-  }
-  
+    </>
+  );
+}
